65d5619b9d3b919a7edd23ea7ee91a9a2bf13a19
Evan-J-Hahn <evan.hahn@me.com>
2020-06-01 11:27:36 -0700

    fix shop selector map functiion

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -7,15 +7,15 @@ import './collection.styles.scss'
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection
 
-   return (
-    <div className='collection-page'>  
+  return (
+    <div className='collection-page'>
       <h2 className='title'>{title}</h2>
       <div className='items'>
         {
-          items.map((item) => (
+          items.map(item => (
             <CollectionItem key={item.id} item={item} />
           ))
-        }
+      }
       </div>
     </div>
   )
